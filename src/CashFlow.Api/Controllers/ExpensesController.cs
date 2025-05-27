b3603ffc4bc10d8504d3e47fc87d7b3d505f43fa
Lucas Flaquer <lucas.flaquer@gmail.com>
2025-05-27 07:56:59 -0300

    feat: error validations

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers;
@@ -11,8 +12,28 @@ public class ExpensesController : ControllerBase
     [HttpPost]
     public IActionResult Register([FromBody] RequestExpenseJson request)
     {
-        var useCase = new RegisterExpenseUseCase();
-        var response = useCase.Execute(request);
-        return Created(string.Empty, response);
+        try
+        {
+            var useCase = new RegisterExpenseUseCase();
+            var response = useCase.Execute(request);
+            return Created(string.Empty, response);
+        }
+        catch (ArgumentException ex)
+        {
+            var errorResponse = new ResponseErrorJson
+            {
+                ErrorMessage = ex.Message
+            };
+            return BadRequest(errorResponse);
+        }
+        catch
+        {
+            var errorResponse = new ResponseErrorJson
+            {
+                ErrorMessage = "An unexpected error occurred."
+            }
+            ;
+            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
+        }
     }
 }

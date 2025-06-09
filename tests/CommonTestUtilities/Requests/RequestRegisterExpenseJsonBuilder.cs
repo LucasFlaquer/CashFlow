451305ccee28ecf28e513bfcf97129985979ce22
Lucas Flaquer <lucas.flaquer@gmail.com>
2025-06-09 13:18:16 -0300

    Add CommonTestUtilities project and data builder class
    - Introduced a new project "CommonTestUtilities" in the solution.
    - Created `CommonTestUtilities.csproj` targeting .NET 8.0 with necessary configurations and dependencies.
    - Added `RequestRegisterExpenseJsonBuilder` class to generate fake data for `RequestExpenseJson` using the Bogus library.

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestRegisterExpenseJsonBuilder
+{
+    public RequestExpenseJson Build()
+    {
+        return new Faker<RequestExpenseJson>()
+            .RuleFor(request => request.Title, faker => faker.Commerce.ProductName())
+            .RuleFor(request => request.Description, faker => faker.Commerce.ProductDescription())
+            .RuleFor(request => request.Date, faker => faker.Date.Past())
+            .RuleFor(request => request.PaymentType, faker => faker.PickRandom<PaymentType>())
+            .RuleFor(request => request.Amount, faker => faker.Random.Decimal(min: 1, max: 1000));
+    }
+}

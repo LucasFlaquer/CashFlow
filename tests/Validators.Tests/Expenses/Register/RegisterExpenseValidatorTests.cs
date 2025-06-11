69007d0eddda41792f8c77753c6d785b853030f5
Lucas Flaquer <lucas.flaquer@gmail.com>
2025-06-11 08:23:26 -0300

    Add tests for RegisterExpenseValidator and update packages
    Enhanced `RegisterExpenseValidatorTests.cs` with new test cases for error conditions including empty title, future date, invalid payment type, and invalid amounts. Updated assertions to use Shouldly for consistency.
    
    Updated package references in `Validators.Tests.csproj` to newer versions for improved compatibility and features.

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,5 +1,8 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
+using CashFlow.Exception;
 using CommonTestUtilities.Requests;
+using Shouldly;
 
 namespace Validators.Tests.Expenses.Register;
 
@@ -11,6 +14,58 @@ public class RegisterExpenseValidatorTests
         var validator = new RegisterExpenseValidator();
         var request = RequestRegisterExpenseJsonBuilder.Build();
         var result = validator.Validate(request);
-        Assert.True(result.IsValid);
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Error_Title_Empty()
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = string.Empty;
+        var result = validator.Validate(request);
+        result.IsValid.ShouldBeFalse();
+        result.Errors.Count.ShouldBe(1);
+        result.Errors[0].ErrorMessage.ShouldBe(ResourceErrorMessages.TITLE_REQUIRED);
+    }
+
+    [Fact]
+    public void Error_Date_Future()
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Date = DateTime.UtcNow.AddDays(1);
+        var result = validator.Validate(request);
+        result.IsValid.ShouldBeFalse();
+        result.Errors.Count.ShouldBe(1);
+        result.Errors[0].ErrorMessage.ShouldBe(ResourceErrorMessages.DATE_CANNOT_FOR_THE_FUTURE);
+    }
+
+    [Fact]
+    public void Error_Payment_Type_Invalid()
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.PaymentType = (PaymentType)9314;
+        var result = validator.Validate(request);
+        result.IsValid.ShouldBeFalse();
+        result.Errors.Count.ShouldBe(1);
+        result.Errors[0].ErrorMessage.ShouldBe(ResourceErrorMessages.PAYMENT_TYPE_INVALID);
+    }
+    
+    //**Equivalente ao it.each do jest**
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-100)]
+    public void Error_Amount_Invalid(decimal amount)
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Amount = amount;
+        var result = validator.Validate(request);
+        result.IsValid.ShouldBeFalse();
+        result.Errors.Count.ShouldBe(1);
+        result.Errors[0].ErrorMessage.ShouldBe(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
     }
 }

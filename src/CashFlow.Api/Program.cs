fc28bd74ee7b1109c59bb159af17b97e9d183ce9
Lucas Flaquer <lucas.flaquer@gmail.com>
2025-07-08 08:20:49 -0300

    Refactor infrastructure setup and database configuration
    - Updated `AddInfrastructure()` in `Program.cs` to accept a configuration parameter for dynamic service configuration.
    - Removed logging settings in `appsettings.Development.json` and added a connection string for MySQL database.
    - Modified `CashFlowDbContext` to include a constructor accepting `DbContextOptions`, removing the hardcoded connection string.
    - Enhanced `DependencyInjectionExtension` to retrieve the connection string from configuration, ensuring proper database context setup.

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMvc(options => options.Filters.Add<ExceptionFilter>());
-builder.Services.AddInfrastructure();
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 var app = builder.Build();
 app.UseMiddleware<CultureMiddleware>();
--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -5,12 +5,8 @@ namespace CashFlow.Infrastructure.DataAccess;
 
 internal class CashFlowDbContext: DbContext
 {
+    public CashFlowDbContext(DbContextOptions options) : base(options) {}
     public DbSet<Expense> Expenses { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        var connectionString = "Server=localhost;port=3306;Database=ignite-cashflow;user=docker;password=docker;";
-        var serverVersion = new MySqlServerVersion(new Version(9,3,0));
-        optionsBuilder.UseMySql(connectionString, serverVersion);
-    }
+   
 }

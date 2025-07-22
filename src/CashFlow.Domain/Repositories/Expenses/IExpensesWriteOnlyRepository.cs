c5cb50b58938ab349dd98cb09bc2cd53920798fa
Lucas Flaquer <lucas.flaquer@gmail.com>
2025-07-22 08:59:18 -0300

    Add delete expense functionality and refactor related code
    - Implemented a new HTTP DELETE endpoint in `ExpensesController.cs` for deleting expenses.
    - Registered `IDeleteExpenseUseCase` in `DependencyInjectionExtension.cs`.
    - Created `DeleteExpenseUseCase` to handle deletion logic and error handling.
    - Added `IDeleteExpenseUseCase` interface to define the delete contract.
    - Updated `IExpensesWriteOnlyRepository` to include a `Delete` method.
    - Refactored `IUnitOfWork` to use asynchronous `Commit` method.
    - Implemented `Delete` method in `ExpensesRepository` for expense removal.
    - Changed `UnitOfWork` to support asynchronous database operations.

--- src/CashFlow.Domain/Repositories/Expenses/IExpensesWriteOnlyRepository.cs
+++ src/CashFlow.Domain/Repositories/Expenses/IExpensesWriteOnlyRepository.cs
@@ -4,4 +4,10 @@ namespace CashFlow.Domain.Repositories.Expenses;
 public interface IExpensesWriteOnlyRepository
 {
     Task Add(Expense expense);
+    /// <summary>
+    /// Deletes an expense by its ID. Returns True if deleted successfully, otherwise returns False.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    Task<bool> Delete(long id);
 }
--- src/CashFlow.Infrastructure/DataAccess/UnitOfWork.cs
+++ src/CashFlow.Infrastructure/DataAccess/UnitOfWork.cs
@@ -10,5 +10,5 @@ internal class UnitOfWork : IUnitOfWork
     {
         _dbContext = dbContext;
     }
-    public void Commit() => _dbContext.SaveChanges();
+    public Task Commit() => _dbContext.SaveChangesAsync();
 }

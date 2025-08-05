e13703d87d5c7e6551e051549e8fce21e0556d04
Lucas Flaquer <lucas.flaquer@gmail.com>
2025-08-05 08:49:02 -0300

    Enhance expenses report generation in Excel
    Added currency symbol constant and improved report logic.
    Introduced `ConvertPaymentType` method for localization.
    Reordered header columns for better alignment with data.

--- src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Domain.Reports;
+﻿using CashFlow.Domain.Enums;
+using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
 using ClosedXML.Excel;
 
@@ -6,6 +7,7 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
 
 public class GenerateExpensesReportExcelUseCase: IGenerateExpensesReportExcelUseCase
 {
+    private const string CURRENCY_SYMBOL = "$";
     private readonly IExpensesReadOnlyRepository _repository;
 
     public GenerateExpensesReportExcelUseCase(IExpensesReadOnlyRepository repository)
@@ -25,18 +27,43 @@ public class GenerateExpensesReportExcelUseCase: IGenerateExpensesReportExcelUse
         workbook.Style.Font.FontName = "Times New Roman";
         var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
         InsertHeader(worksheet);
+        
+        var row = 2;
+        foreach (var expense in expenses)
+        {
+            worksheet.Cell($"A{row}").Value = expense.Title;
+            worksheet.Cell($"B{row}").Value = expense.Date;
+            worksheet.Cell($"C{row}").Value = ConvertPaymentType(expense.PaymentType);
+            worksheet.Cell($"D{row}").Value = expense.Amount;
+            worksheet.Cell($"D{row}").Style.NumberFormat.Format = $"{CURRENCY_SYMBOL} #,##0.00";
+            worksheet.Cell($"E{row}").Value = expense.Description;
+            row++;
+        }
+        worksheet.Columns().AdjustToContents();
+
         var file = new MemoryStream();
         workbook.SaveAs(file);
-
         return file.ToArray();
+    }
+
+    private string ConvertPaymentType(PaymentType payment)
+    {
+        return payment switch
+        {
+            PaymentType.Cash =>"Dinehiro",
+            PaymentType.CreditCard => "Cartão de Crédito",
+            PaymentType.DebitCard => "Cartão de Débito",
+            PaymentType.ElectronicTransfer => "Trasferência Bancária",
+            _ => string.Empty
+        };
     }
 
     private void InsertHeader(IXLWorksheet worksheet)
     {
         worksheet.Cell("A1").Value = ResourceReportGenerationMessage.TITLE;
         worksheet.Cell("B1").Value = ResourceReportGenerationMessage.DATE;
-        worksheet.Cell("C1").Value = ResourceReportGenerationMessage.AMOUNT;
-        worksheet.Cell("D1").Value = ResourceReportGenerationMessage.PAYMENT_TYPE;
+        worksheet.Cell("C1").Value = ResourceReportGenerationMessage.PAYMENT_TYPE;
+        worksheet.Cell("D1").Value = ResourceReportGenerationMessage.AMOUNT;
         worksheet.Cell("E1").Value = ResourceReportGenerationMessage.DESCRIPTION;
         worksheet.Cells("A1:E1").Style.Font.Bold = true;
         worksheet.Cells("A1:E1").Style.Fill.BackgroundColor = XLColor.FromHtml("#F5C2B6");

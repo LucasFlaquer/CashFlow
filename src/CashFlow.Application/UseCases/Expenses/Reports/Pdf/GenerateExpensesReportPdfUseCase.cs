21f721e27a7b0adb5577e862d178c6b4509a4bc3
Lucas Flaquer <lucas.flaquer@gmail.com>
2025-08-19 08:33:47 -0300

    Add logo support and refactor PDF report generation
    Updated `CashFlow.Application.csproj` to include a logo image (`spyro.jpeg`) for output. Added the binary file `spyro.jpeg`. Refactored `GenerateExpensesReportPdfUseCase.cs` to improve PDF generation by introducing methods for header and total expenses sections, and replaced hardcoded image paths with dynamic paths for better maintainability.

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,9 +1,11 @@
-﻿ using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+﻿using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
 using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
 using MigraDoc.DocumentObjectModel;
 using MigraDoc.Rendering;
 using PdfSharp.Fonts;
+using System.Reflection;
+using System.Security.AccessControl;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 
@@ -23,26 +25,9 @@ public class GenerateExpensesReportPdfUseCase: IGenerateExpensesReportPdfUseCase
         if (expenses.Count == 0) return [];
         var document = CreateDocument(month);
         var page = CreatePage(document);
-        // Page header with image
-        var table = page.AddTable();
-        table.AddColumn();
-        table.AddColumn("300");
-        var row = table.AddRow();
-        var img = row.Cells[0].AddImage("C:\\Users\\lucas.flaquer\\Desktop\\sypro.jpeg");
-        img.Width = "62";
-        img.Height = "62";
-        img.LockAspectRatio = true;
-        row.Cells[1].AddParagraph("Hey, Lucas Flaquer");
-        row.Cells[1].Format.Font = new Font { Name = FontHelper.RALLEWAY_BLACK, Size = 16 };
-        row.Cells[1].VerticalAlignment = MigraDoc.DocumentObjectModel.Tables.VerticalAlignment.Center;
-        // PageHeader
-        var paragraph = page.AddParagraph();
-        paragraph.Format.SpaceBefore = "40";
-        var title = string.Format(ResourceReportGenerationMessage.TOTAL_SPENT_IN, month.ToString("Y"));
-        paragraph.AddFormattedText(title, new Font { Name = FontHelper.RALLEWAY_REGULAR, Size = 15 });
-        paragraph.AddLineBreak();
+        CreateHeaderWithProfilePhotoAndName(page);
         var totalExpenses = expenses.Sum(expense => expense.Amount);
-        paragraph.AddFormattedText($"{totalExpenses}{CURRENCY_SYMBOL}", new Font { Name = FontHelper.WORKSANS_BLACK, Size = 50 });
+        CreateTotalSpentSection(page, month, totalExpenses);
         return RenderDocument(document);
     }
 
@@ -80,5 +65,35 @@ public class GenerateExpensesReportPdfUseCase: IGenerateExpensesReportPdfUseCase
         using var file = new MemoryStream();
         renderer.PdfDocument.Save(file);
         return file.ToArray();
+    }
+
+    private void CreateHeaderWithProfilePhotoAndName(Section page)
+    {
+        // Page header with image
+        var table = page.AddTable();
+        table.AddColumn();
+        table.AddColumn("300");
+        var row = table.AddRow();
+        var assembly = Assembly.GetExecutingAssembly();
+        var directoryName = Path.GetDirectoryName(assembly.Location);
+        var pathFile = Path.Combine(directoryName!, "Logo", "spyro.speg");
+        var img = row.Cells[0].AddImage(pathFile);
+        img.Width = "62";
+        img.Height = "62";
+        img.LockAspectRatio = true;
+        row.Cells[1].AddParagraph("Hey, Lucas Flaquer");
+        row.Cells[1].Format.Font = new Font { Name = FontHelper.RALLEWAY_BLACK, Size = 16 };
+        row.Cells[1].VerticalAlignment = MigraDoc.DocumentObjectModel.Tables.VerticalAlignment.Center;
+    }
+
+    private void CreateTotalSpentSection(Section page, DateOnly month, decimal totalExpenses)
+    {
+        var paragraph = page.AddParagraph();
+        paragraph.Format.SpaceBefore = "40";
+        var title = string.Format(ResourceReportGenerationMessage.TOTAL_SPENT_IN, month.ToString("Y"));
+        paragraph.AddFormattedText(title, new Font { Name = FontHelper.RALLEWAY_REGULAR, Size = 15 });
+        paragraph.AddLineBreak();
+        
+        paragraph.AddFormattedText($"{totalExpenses}{CURRENCY_SYMBOL}", new Font { Name = FontHelper.WORKSANS_BLACK, Size = 50 });
     }
 }
